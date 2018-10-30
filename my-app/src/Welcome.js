ebd2cf03169b9e603bd59da235fa6a60858c984e
徐金俊 <xujj@anchnet.com>
2018-10-30 22:26:38 +0800

    props

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,7 +1,9 @@
 import React from 'react'; // 为什么要 import React
 class Welcome extends React.Component {
   render() {
-    return <h1 > Hello, Component < /h1>;
+    return <h1 > Hello, {
+      this.props.name
+    } < /h1>;
   }
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import Welcome from './Welcome'
 
 ReactDOM.render( <
-  Welcome / > ,
+  Welcome name = "xujinjun" / > ,
   document.getElementById('root')
 )
 
