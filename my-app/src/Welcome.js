5aadffbe13ae27922e22e59a6d8eb438ee5aa0c9
徐金俊 <xujj@anchnet.com>
2018-10-30 22:21:21 +0800

    welcome组件

---
+++ my-app/src/Welcome.js
@@ -0,0 +1,8 @@
+import React from 'react'; // 为什么要 import React
+class Welcome extends React.Component {
+  render() {
+    return <h1 > Hello, Component < /h1>;
+  }
+}
+
+export default Welcome // 为什么要 export，为什么要加 default
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Welcome from './Welcome'
 
-ReactDOM.render( < h1 > Hi, world! < /h1>, document.getElementById('root2'));
+ReactDOM.render( <
+  Welcome / > ,
+  document.getElementById('root')
+)
 
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: http://bit.ly/CRA-PWA
-    serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
