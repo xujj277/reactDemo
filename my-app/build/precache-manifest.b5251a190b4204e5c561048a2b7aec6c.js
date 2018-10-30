391daf4f26b45f6b345dc7d7e4f46fcc14a4f5cf
徐金俊 <xujj@anchnet.com>
2018-10-30 21:28:37 +0800

    build

---
+++ my-app/build/precache-manifest.b5251a190b4204e5c561048a2b7aec6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactDemo//my-app/build/index.html/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "225b6ab1715d8a807c48",
+    "url": "/reactDemo//my-app/build/index.html/static/js/runtime~main.225b6ab1.js"
+  },
+  {
+    "revision": "4135cacc672011751d7c",
+    "url": "/reactDemo//my-app/build/index.html/static/js/main.4135cacc.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/reactDemo//my-app/build/index.html/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "4135cacc672011751d7c",
+    "url": "/reactDemo//my-app/build/index.html/static/css/main.158d5b47.chunk.css"
+  },
+  {
+    "revision": "8b4ce658b218c4b7cf0fb2ae22d8c812",
+    "url": "/reactDemo//my-app/build/index.html/index.html"
+  }
+];
--- my-app/build/service-worker.js
+++ my-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fca106eda9d6380864b18ceafe0ee0ab.js"
+  "/reactDemo//my-app/build/index.html/precache-manifest.b5251a190b4204e5c561048a2b7aec6c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactDemo//my-app/build/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
