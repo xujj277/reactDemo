5553b217b76417a5fdcf12583884ac926a5298b0
徐金俊 <xujj@anchnet.com>
2018-10-30 22:36:58 +0800

    state 的用法

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,9 +1,23 @@
 import React from 'react'; // 为什么要 import React
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
   render() {
-    return <h1 > Hello, {
-      this.props.name
-    } < /h1>;
+    return ( <
+      div >
+      <
+      h1 > Hello, {
+        this.props.name
+      } < /h1> <
+      h2 > {
+        this.state.date.toString()
+      } < /h2> < /
+      div >
+    );
   }
 }
 
