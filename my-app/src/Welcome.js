539176ff4235161428e0546f01e4495508e1780a
徐金俊 <xujj@anchnet.com>
2018-10-31 10:15:32 +0800

    想知道能在哪些钩子里setState

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -5,17 +5,24 @@ class Welcome extends React.Component {
     this.state = {
       date: new Date()
     }
-    setInterval(() => { // 搜索「JS 箭头函数 MDN」
-      this.setState({
-        date: new Date() // 更新 date
-      })
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'constructor'
     })
     console.log('我已经在 constructor 里将 props 和 state 初始化好了')
   }
   componentWillMount() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentWillMount'
+    })
     console.log('运行到这里的话，说明马上就要运行 render 了')
   }
   render() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'render'
+    })
     console.log('嗯，这里是 render')
     return ( <
       div >
@@ -30,8 +37,41 @@ class Welcome extends React.Component {
     );
   }
   componentDidMount() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentDidMount'
+    })
     console.log('已经挂载到页面里了')
   }
+  componentWillReceiveProps() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentWillReceiveProps'
+    })
+  }
+  shouldComponentUpdate() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'shouldComponentUpdate'
+    })
+    return true
+  }
+  componentWillUpdate() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentWillUpdate'
+    })
+  }
+  componentDidUpdate() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentDidUpdate'
+    })
+  }
+  componentWillUnmount() {
+    console.log('要死了')
+  }
+
 }
 
 export default Welcome // 为什么要 export，为什么要加 default
