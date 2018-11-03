aac035bccdb1434646c8ca1a3c45de31a22fa4a0
徐金俊 <xujj@anchnet.com>
2018-11-03 23:06:36 +0800

    fix value again

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,17 +9,14 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'test',
-      todoList: [
-        {id:1, title:'第一个待办'},
-        {id:2, title:'第二个待办'},
-      ]
+      newTodo: '',
+      todoList: []
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
       return ( // 为什么这里要加个括号？这是动手题3 🐸
-        <li>
+        <li key={index} >
           <TodoItem todo={item} />
         </li>
       )
@@ -28,7 +25,9 @@ class App extends Component {
       <div className="App">
           <h1>我的待办</h1>
           <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+          <TodoInput content={this.state.newTodo} 
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
           </div>
         <ol>
         {todos}
@@ -36,9 +35,31 @@ class App extends Component {
       </div>
     )
   }
-  addTodo(){
-    console.log('我得添加一个 todo 了')
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
   }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  
 }
 
 export default App;
+
+let id = 0
+ function idMaker(){
+  id += 1
+  return id
+}
