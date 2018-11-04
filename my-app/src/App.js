535e6310d2ef77e59300099ab2e03fee7c47d01b
徐金俊 <xujj@anchnet.com>
2018-11-04 22:39:55 +0800

    将代码移到componentDidUpdate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -38,6 +38,9 @@ class App extends Component {
       </div>
     )
   }
+  componentDidUpdate(){
+    localStore.save('todoList', this.state.todoList)
+  }
   addTodo(event){
     this.state.todoList.push({
       id: idMaker(),
@@ -49,24 +52,20 @@ class App extends Component {
       newTodo: '',
       todoList: this.state.todoList
     })
-    localStore.save('todoList', this.state.todoList)
   }
   toggle(e, todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
     this.setState(this.state) 
-    localStore.save('todoList', this.state.todoList)
   }
   changeTitle(event){
     this.setState({
       newTodo: event.target.value,
       todoList: this.state.todoList
     })
-    localStore.save('todoList', this.state.todoList)
   }
   delete(event, todo){
     todo.deleted = true
     this.setState(this.state) 
-    localStore.save('todoList', this.state.todoList)
   }
 }
 
