14daa0a1d1ff395f40dc235fb94d654fa1a895b0
徐金俊 <xujj@anchnet.com>
2018-11-04 15:05:22 +0800

    删除todo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
     let todos = this.state.todoList.map((item,index)=>{
       return ( // 为什么这里要加个括号？这是动手题3 🐸
         <li key={index} >
-          <TodoItem todo={item} onToggle={this.toggle.bind(this)} />
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} 
+            onDelete={this.delete.bind(this)}/>
         </li>
       )
     })
@@ -57,7 +58,10 @@ class App extends Component {
       todoList: this.state.todoList
     })
   }
-  
+  delete(event, todo){
+    todo.deleted = true
+    this.setState(this.state) 
+  }
 }
 
 export default App;
