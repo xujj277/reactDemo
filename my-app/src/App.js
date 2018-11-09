ae99792593c021c442048d25cf586172836a5f79
徐金俊 <xujj@anchnet.com>
2018-11-09 16:41:29 +0800

    合并 onSignUp 和 onSignIn

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -42,18 +42,12 @@ class App extends Component {
         {this.state.user.id ? 
           null : 
           <UserDialog 
-            onSignUp={this.onSignUp.bind(this)} 
-            onSignIn={this.onSignIn.bind(this)}/>}
-      </div>
+          onSignUp={this.onSignUpOrSignIn.bind(this)} 
+          onSignIn={this.onSignUpOrSignIn.bind(this)}/>}      
+        </div>
     )
   }
-  signOut(){
-    signOut()
-    let stateCopy = JSON.parse(JSON.stringify(this.state))
-    stateCopy.user = {}
-    this.setState(stateCopy)
-  }
-  onSignUp(user){
+  onSignUpOrSignIn(user){
     let stateCopy = JSON.parse(JSON.stringify(this.state)) 
     stateCopy.user = user
     this.setState(stateCopy)
