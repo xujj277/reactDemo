83ebc8a09dc44640dcb018521690cef395eba497
徐金俊 <xujj@anchnet.com>
2018-11-13 00:33:29 +0800

    将 SignUpForm 抽离成一个组件（重要）

--- my-app/src/UserDialog.js
+++ my-app/src/UserDialog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './UserDialog.css'
+import SignUpForm from './SignUpForm'
 import {signUp, signIn, sendPasswordResetEmail} from './leanCloud'
 export default class UserDialog extends Component{
   constructor(props){
@@ -61,28 +62,6 @@ export default class UserDialog extends Component{
     })
   }
   render(){
-    let signUpForm = (
-      <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
-        <div className="row">
-          <label>邮箱</label> 
-          <input type="text" value={this.state.formData.email}
-            onChange={this.changeFormData.bind(this, 'email')}/>
-        </div>
-        <div className="row">
-          <label>用户名</label> 
-          <input type="text" value={this.state.formData.username}
-            onChange={this.changeFormData.bind(this, 'username')}/>
-        </div>
-        <div className="row">
-          <label>密码</label>
-          <input type="password" value={this.state.formData.password} 
-            onChange={this.changeFormData.bind(this, 'password')}/>
-        </div>
-        <div className="row actions">
-          <button type="submit">注册</button>
-        </div>
-      </form>
-    )
     let signInForm = (
       <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
         <div className="row">
@@ -116,7 +95,11 @@ export default class UserDialog extends Component{
             /> 登录</label>
         </nav>
         <div className="panes">
-          {this.state.selected === 'signUp' ? signUpForm : null}
+        {this.state.selected === 'signUp' ?
+            <SignUpForm formData={this.state.formData}
+              onSubmit={this.signUp.bind(this)}
+              onChange={this.changeFormData.bind(this)}/>
+            : null}
           {this.state.selected === 'signIn' ? signInForm : null}
         </div>
       </div>
