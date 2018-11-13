bec78dae8e65c5ea9b4440ccd147ece9e84bcb3b
徐金俊 <xujj@anchnet.com>
2018-11-13 22:47:36 +0800

    如果用户在输入框里什么都不写就敲回车，那么就拒绝用户

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,21 @@ import './reset.css'
 import UserDialog from './UserDialog'
 import {getCurrentUser, signOut} from './leanCloud'
 
+import AV from './leanCloud'
+// 声明一个 Todo 类型
+var Todo = AV.Object.extend('Todo');
+// 新建一个 Todo 对象
+var todo = new Todo();
+todo.set('title', '工程师周会');
+todo.set('content', '每周工程师会议，周一下午2点');
+todo.save().then(function (todo) {
+  // 成功保存之后，执行其他逻辑.
+  console.log('New object created with objectId: ' + todo.id);
+}, function (error) {
+  // 异常处理
+  console.error('Failed to create new object, with error message: ' + error.message);
+});
+
 class App extends Component {
   constructor(props){
     super(props)
