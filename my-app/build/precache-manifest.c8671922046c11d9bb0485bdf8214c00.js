33c8215a1c4e63697dce9dece75ade1150a724a5
徐金俊 <xujj@anchnet.com>
2018-11-15 17:36:01 +0800

    npm run build

---
+++ my-app/build/precache-manifest.c8671922046c11d9bb0485bdf8214c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b042bed13c01d1cd4d7",
+    "url": "/reactDemo/my-app/build/static/js/runtime~main.0b042bed.js"
+  },
+  {
+    "revision": "8f12a6ee822999d40dc0",
+    "url": "/reactDemo/my-app/build/static/js/main.8f12a6ee.chunk.js"
+  },
+  {
+    "revision": "69a7d800e5479c0f05c2",
+    "url": "/reactDemo/my-app/build/static/js/1.69a7d800.chunk.js"
+  },
+  {
+    "revision": "8f12a6ee822999d40dc0",
+    "url": "/reactDemo/my-app/build/static/css/main.e825d721.chunk.css"
+  },
+  {
+    "revision": "69a7d800e5479c0f05c2",
+    "url": "/reactDemo/my-app/build/static/css/1.db8368b1.chunk.css"
+  },
+  {
+    "revision": "6fa23ddec3c7110afef649458c19270b",
+    "url": "/reactDemo/my-app/build/index.html"
+  }
+];
